f363ab5a6f972610e1b423fc6e39175ed73cdf8d
alexandrYun <alexandryun@mail.ru>
2020-10-14 22:01:29 +0300

    first

---
+++ Todo.js
@@ -0,0 +1,14 @@
+export class Todo {
+    /*
+        priority - важность дела
+        text - контент
+        dt - дата создания
+        dl -  deadline
+    */
+    constructor(priority, text, dl) {
+        this.priority = priority;
+        this.text = text;
+        this.dt = Date.now();
+        this.dl = dl;
+    }
+}
---
+++ script.js
@@ -0,0 +1,4 @@
+import Todo from './Todo';
+
+let todo = new Todo(1, 'erwqew', '15:10');
+console.log(todo);
